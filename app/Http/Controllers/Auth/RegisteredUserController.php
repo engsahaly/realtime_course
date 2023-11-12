8281f8d56ccd565c767ca5e78fc38f58f9698fc0
engsahaly <engsahaly@gmail.com>
2023-11-13 00:38:06 +0200

    starting with pusher broadcasting

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\NewUserRegisteredEvent;
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
 use App\Models\User;
@@ -11,6 +12,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Validation\Rules;
@@ -54,6 +56,10 @@ class RegisteredUserController extends Controller
 
         // $admins = Admin::all();
         // Notification::send(Admin::all(), new NewUserRegisteredNotification($user));
+
+        // BROADCAST EVENT
+        NewUserRegisteredEvent::dispatch($user);
+        // Broadcast(new NewUserRegisteredEvent());
 
         Auth::login($user);
 
--- resources/views/dashboard/partials/head.blade.php
+++ resources/views/dashboard/partials/head.blade.php
@@ -25,4 +25,23 @@
     <link rel="stylesheet" href="{{ asset('assets') }}/css/app-light.css" id="lightTheme">
     <link rel="stylesheet" href="{{ asset('assets') }}/css/app-dark.css" id="darkTheme" disabled>
     @stack('styles')
+
+
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <script>
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = false;
+
+        var pusher = new Pusher("{{ env('PUSHER_APP_KEY') }}", {
+            cluster: 'mt1'
+        });
+
+        var channel = pusher.subscribe('new_user_channel');
+
+        channel.bind('App\\Events\\NewUserRegisteredEvent', function(data) {
+            console.log(data['message']);
+            $(".notificationsIcon").load(" .notificationsIcon > *");
+            $("#notificationsModal").load(" #notificationsModal > *");
+        });
+    </script>
 </head>
